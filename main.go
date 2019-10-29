66238bf0a14a398fdfcf9722803689164b321cf7
wangyl <wangyongl@irisking.com>
2019-10-29 10:48:00 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"flag"
+	"fmt"
+	"os"
+	"test-build-info/bininfo"
+)
+
+func main() {
+	v := flag.Bool("v", false, "show bin info")
+	flag.Parse()
+	if *v {
+		_, _ = fmt.Fprint(os.Stderr, bininfo.StringifyMultiLine())
+		os.Exit(1)
+	}
+
+	fmt.Println("my app running...")
+	fmt.Println("bye...")
+}
